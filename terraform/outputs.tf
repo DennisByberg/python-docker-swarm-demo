d313fb2e9abeb0e9a89e3ee5c8c26c416cd638c9
dennisbyberg <dennis.byberg@hotmail.com>
2025-09-09 18:01:20 +0200

    add AWS storage infrastructure with S3 and DynamoDB

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -52,3 +52,13 @@ output "fastapi_target_group_arn" {
   description = "ARN of the FastAPI target group"
   value       = aws_lb_target_group.fastapi.arn
 }
+
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket for image storage"
+  value       = aws_s3_bucket.image_uploads.id
+}
+
+output "dynamodb_table_name" {
+  description = "Name of the DynamoDB table for posts"
+  value       = aws_dynamodb_table.posts.name
+}
